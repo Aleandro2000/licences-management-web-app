f86e6480d12efcafc6ce7eed8e0fa27c4464f66f
Aleandro2000 <carmicialexandru@yahoo.com>
2021-10-21 04:40:19 +0300

    licences

--- back/src/main.ts
+++ back/src/main.ts
@@ -3,7 +3,13 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import {createDatabase} from "typeorm-extension";
 import { AppModule } from './app.module';
+
+(async () => {
+  await createDatabase({ifNotExist: true, charset: "utf8mb4_general_ci", characterSet: "utf8mb4"});
+  process.exit(0);
+})();
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
