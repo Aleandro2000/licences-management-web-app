3fd0dd29c972d4fef275c067aa750014f2441a4e
Aleandro2000 <carmicialexandru@yahoo.com>
2021-10-23 18:03:18 +0300

    licences

--- back/src/users/entities/user.entity.ts
+++ back/src/users/entities/user.entity.ts
@@ -2,27 +2,6 @@ import { Entity,Column,PrimaryGeneratedColumn,BaseEntity,OneToMany,ManyToOne } f
 import { University } from "../../universities/entities/university.entity";
 
 @Entity()
-export class Teacher extends BaseEntity {
-    @PrimaryGeneratedColumn("increment")
-    id: number;
-
-    @Column()
-    username: string;
-
-    @Column()
-    email: string;
-
-    @Column()
-    password: string;
-
-    @ManyToOne(() => Student, student => student.teacher)
-    student: Student[];
-    
-    @ManyToOne(() => University, university => university.student)
-    university: University[];
-}
-
-@Entity()
 export class Student extends BaseEntity {
     @PrimaryGeneratedColumn("increment")
     id: number;
@@ -36,8 +15,29 @@ export class Student extends BaseEntity {
     @Column()
     password: string;
 
-    @OneToMany(() => Teacher, teacher => teacher.student)
-    teacher: Teacher;
+    @ManyToOne(() => Teacher, teacher => teacher.student)
+    teacher: Teacher[];
+    
+    @ManyToOne(() => University, university => university.student)
+    university: University[];
+}
+
+@Entity()
+export class Teacher extends BaseEntity {
+    @PrimaryGeneratedColumn("increment")
+    id: number;
+
+    @Column()
+    username: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @OneToMany(() => Student, student => student.teacher)
+    student: Student;
     
     @ManyToOne(() => University, university => university.student)
     university: University[];
