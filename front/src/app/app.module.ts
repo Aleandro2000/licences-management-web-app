a037b6ae63d421dfe02902409f2ac4a1d21b7739
Aleandro2000 <carmicialexandru@yahoo.com>
2021-10-25 13:32:45 +0300

    licences

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
