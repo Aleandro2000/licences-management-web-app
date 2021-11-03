02cfeaa0a4512766bb7aca4fec790b138d9ab907
Alexandru Carmici <66217150+Aleandro2000@users.noreply.github.com>
2021-11-04 01:40:10 +0200

    licences

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -1,32 +1,41 @@
 import type { NextPage } from 'next'
+import Link from "next/link"
 import { isLogin } from "../utils"
 
 const Navbar: NextPage = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light p-3">
       <span className="navbar-brand">LICENCE APP</span>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="true" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"/>
       </button>
       <div className="collapse navbar-collapse" id="navbarText">
         <ul className="navbar-nav ms-auto text-center">
-          <li className="nav-item">
-            <a className="nav-link" href="#">Home</a>
-          </li>
+          <Link href="/" passHref={true}>
+            <li className="nav-item nav-link" style={{cursor: "pointer"}}>
+              Home
+            </li>
+          </Link>
           {
               !isLogin() ? (
                 <>
-                  <li className="nav-item">
-                    <a className="nav-link" href="#">Login</a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="#">Register</a>
-                  </li>
+                  <Link href="/login" passHref={true}>
+                    <li className="nav-item nav-link" style={{cursor: "pointer"}}>
+                      Login
+                    </li>
+                  </Link>
+                  <Link href="/login" passHref={true}>
+                    <li className="nav-item nav-link" style={{cursor: "pointer"}}>
+                      Register
+                    </li>
+                  </Link>
                 </>
               ) : (
-                <li className="nav-item">
-                  <a className="nav-link" href="#">Dashboard</a>
-                </li>
+                <Link href="/dashboard" passHref={true}>
+                  <li className="nav-item nav-link" style={{cursor: "pointer"}}>
+                    Dashboard
+                  </li>
+                </Link>
               )
           }
         </ul>
