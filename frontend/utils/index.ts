805d089f46cbed8b32df1e10ad8d996086d10281
Alexandru Carmici <66217150+Aleandro2000@users.noreply.github.com>
2021-11-05 09:50:52 +0200

    licences

--- frontend/utils/index.ts
+++ frontend/utils/index.ts
@@ -8,11 +8,6 @@ export function logout()
     localStorage.removeItem("session");
 }
 
-export function getSession()
-{
-    return JSON.parse(localStorage.getItem("session"));
-}
-
 export function isLogin()
 {
     if(localStorage.getItem("session"))
