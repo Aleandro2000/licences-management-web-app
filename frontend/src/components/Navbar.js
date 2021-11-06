be6ad48f83e15a35c164acdad96e891d9bb2b42f
Aleandro2000 <carmicialexandru@yahoo.com>
2021-11-06 21:58:02 +0200

    licences

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar()
     return(
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <span className="navbar-brand">LICENCES APP</span>
+                <span className="navbar-brand"><i className="fa fa-graduation-cap"/></span>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
