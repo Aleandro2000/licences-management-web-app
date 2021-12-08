4698f5954fb4b157c2bcf71991a97c30b83ab6de
Alexandru Carmici <66217150+Aleandro2000@users.noreply.github.com>
2021-12-09 01:54:16 +0200

    licences

--- frontend/src/components/Pannel.js
+++ frontend/src/components/Pannel.js
@@ -2,13 +2,20 @@ export default function Pannel(props) {
     switch (props.type) {
         case "student":
             return (
-                <>
-                </>
+                <div className="container mt-5">
+                    <select className="form-select" defaultValue={""} id="type" name="type">
+                        <option value="">SELECT UNIVERSITY</option>
+                        <option value="upb">UPB</option>
+                        <option value="ase">ASE</option>
+                        <option value="unibuc">UniBuc</option>
+                        <option value="utcb">UTCB</option>
+                    </select>
+                </div>
             );
         case "teacher":
             return (
-                <>
-                </>
+                <div className="container">
+                </div>
             );
         default:
             return (
