6ae2f0f37bda19b7d59912292a0adad8e3f98651
alexandruflipside <alexandru.carmici@flipsidegroup.com>
2021-12-16 14:37:10 +0200

    licences

--- frontend/src/components/pannel_component/Licence.jsx
+++ frontend/src/components/pannel_component/Licence.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Licence() {
     const handleSubmit = async e => {
-        e.prevendDefault();
+        e.preventDefault();
     }
 
     return (
