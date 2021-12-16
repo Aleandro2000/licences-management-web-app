92eaf42ff8a5f08065d002dcb965b8fdd74ad870
alexandruflipside <alexandru.carmici@flipsidegroup.com>
2021-12-16 15:48:23 +0200

    licences

--- frontend/src/components/pannel_component/Licence.jsx
+++ frontend/src/components/pannel_component/Licence.jsx
@@ -1,12 +1,31 @@
 import React, { useContext, useState } from "react";
-import { UniversitiesContext } from "../../context/UserContext";
+import { UniversitiesContext, UserContext } from "../../context/UserContext";
+import { getCookie } from "../../utils";
 
 export default function Licence() {
+    const [user, setUser] = useContext(UserContext);
     const [universities, setUniversities] = useContext(UniversitiesContext);
     const [university, setUniversity] = useState(universities[0]);
+    const [message, setMessage] = useState("");
 
     const handleSubmit = async e => {
         e.preventDefault();
+        await fetch("/licence/upload", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+                "Authorization": "Bearer " + getCookie("jwt")
+            },
+            body: JSON.stringify({
+                studentId: user.result.id,
+                universityId: e.target.university.value,
+                title: e.target.title.value,
+                content: e.target.content.value
+            })
+        })
+            .then(response => response.json())
+            .then(data => setMessage(data.message))
+            .catch(err => setMessage(err));
     }
 
     const handleChange = e => {
@@ -21,25 +40,25 @@ export default function Licence() {
                     Title
                 </b>
             </label>
-            <input type="text" className="form-control" id="title" placeholder="WRITE LICENCE TITLE HERE" />
+            <input type="text" className="form-control" id="title" name="title" placeholder="WRITE LICENCE TITLE HERE" required />
             <br />
-            <label htmlFor="licence">
+            <label htmlFor="content">
                 <b>
                     Send Licence
                 </b>
             </label>
             <br />
-            <textarea className="form-control" id="licnece" />
+            <textarea className="form-control" id="content" name="content" required />
             <br />
             {
                 universities.length ? (
                     <>
-                        <label htmlFor="type">
+                        <label htmlFor="university">
                             <b>
                                 Select Unviersity
                             </b>
                         </label>
-                        <select aria-label='University' className="form-select" defaultValue={university} onChange={handleChange} id="type" name="type">
+                        <select aria-label='University' className="form-select" defaultValue={university} onChange={handleChange} id="university" name="university">
                             {
                                 universities.map((item, index) => {
                                     return (
@@ -59,6 +78,17 @@ export default function Licence() {
             <button type="submit" className="btn btn-dark">
                 <i className="fa fa-send" /> SEND
             </button>
+            <br /><br />
+            <b>
+                {
+                    message ? (
+                        <div className="alert alert-dark" role="alert">
+                            {message}
+                        </div>
+                    ) : (<></>)
+                }
+            </b>
+            <br />
         </form>
     )
 }
