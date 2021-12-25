1301b47210389f721cb6ee195be8e771821199cb
alexandruflipside <alexandru.carmici@flipsidegroup.com>
2021-12-25 05:16:05 +0200

    lincences

---
+++ frontend/src/components/pannel_component/Users.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Users(props) {
+    switch (props.type) {
+        case "student":
+            return (
+                <>
+                </>
+            );
+        case "teacher":
+            return (
+                <>
+                </>
+            );
+        default:
+            return (
+                <>
+                </>
+            );
+    }
+}
